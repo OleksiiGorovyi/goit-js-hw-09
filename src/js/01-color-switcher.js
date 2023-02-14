1acd4a8a39a3b23df30696156825f00350c0a1f3
OleksiiGorovyi <alekseygorovoyyyy@gmail.com>
2023-02-14 22:09:41 +0200

    hw9  1task
    update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bgColor = document.querySelector('body')
+
+console.log(bgColor)
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        let i = getRandomHexColor();
+        bgColor.style.backgroundColor = i;
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener("click", () => {
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+});
